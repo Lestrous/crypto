bfb096cc4520bc5372dbbb3e7351c8fd80bd3903
Lestrous <shtol.leonid@mail.ru>
2021-10-30 18:05:06 +0500

    fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-export default (express, bodyParser, createReadStream, crypto, http, fs, Busboy, inspect, busboyBodyParser) => {
+export default (express, bodyParser, crypto, busboyBodyParser) => {
     const app = express();
 
     const CORS = {
@@ -10,74 +10,16 @@ export default (express, bodyParser, createReadStream, crypto, http, fs, Busboy,
     app
         .use(busboyBodyParser())
         .use((r, res, next) => { r.res.set(CORS); next(); })
-        // .use(bodyParser.urlencoded({ extended: true }))
-        // .use(bodyParser.json())
 
         .post('/', (req, res) => {
-            // let busboy = new Busboy({headers: req.headers});
-            //
-            // busboy.on('field', (fieldname, val) => {
-            //     console.log(fieldname + ': ' + inspect(val));
-            // });
-
-            // req.pipe(busboy);
-
-            // console.log(req.body);
-            // console.log(req.body.key);
-            // console.log(req.body.secret);
-
-
             const key = req.files.key;
             const secret = req.files.secret;
-
-            // console.log(req.body);
-            // console.log(req.body.key);
-            // console.log(req.body.secret);
-            // console.log(req.files);
-            // console.log(key);
-            // console.log(secret);
 
             let buffer = Buffer.from(secret.data, "base64");
             let bufferKey = Buffer.from(key.data, "base64");
             let decrypted = crypto.privateDecrypt(bufferKey, buffer);
 
             res.send(decrypted.toString("utf8"));
-
-            // res.send();
-
-            // if (req.body) {
-            //     const key = req.body.key;
-            //     const secret = req.body.secret;
-            //
-            //     if (key && secret) {
-            //         let buffer = Buffer.from(secret, "base64");
-            //         let decrypted = crypto.privateDecrypt(key, buffer);
-            //
-            //         res.send(decrypted.toString("utf8"));
-            //     } else {
-            //         res.send();
-            //     }
-            // } else {
-            //     res.send();
-            // }
-
-
-            // console.log(key);
-            // console.log(secret);
-            //
-            // const keyName = 'id_rsa2';
-            // const messageName = 'secret2';
-            //
-            // let absolutePath = './' + keyName;
-            // let privateKey = fs.readFileSync(absolutePath);
-            //
-            // let absolutePathMessage = './' + messageName;
-            // let privateMessage = fs.readFileSync(absolutePathMessage);
-            //
-            // let buffer = Buffer.from(privateMessage, "base64");
-            // let decrypted = crypto.privateDecrypt(privateKey, buffer);
-            //
-            // res.send(decrypted.toString("utf8"));
         })
         .get('/login/', (req, res) => res
             .send('Штоль')
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import fs, { createReadStream } from 'fs';
 import crypto from 'crypto';
-import http from 'http';
-import Busboy from 'busboy';
-import { inspect } from 'util';
 import busboyBodyParser from 'busboy-body-parser';
 
 import appSrc from './app.js';
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, fs, Busboy, inspect, busboyBodyParser);
+const app = appSrc(express, bodyParser, crypto, busboyBodyParser);
 
 app.listen(process.env.PORT ?? 4321);
